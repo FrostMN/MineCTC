9a5b56b114baedb6433f2375a4ccfa90d9a95c0e
aaron <asouer@gmail.com>
2018-01-12 10:11:05 -0600

    added css

--- MineCTC/__init__.py
+++ MineCTC/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -6,6 +6,38 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
+
+@app.route("/downloads/mods/<path>")
+def download_mod(path=None):
+    if path is None:
+        return "400"
+    try:
+        print("in try")
+        print(path)
+        file_path = "static/downloads/mods/" + str(path)
+        return send_file(file_path, as_attachment=True)
+    except Exception as e:
+        # self.log.exception(e)
+        # self.Error(400)
+        print(str(e))
+        return "400"
+
+
+@app.route("/downloads/forge/<file>")
+def download_modload(file=None):
+    if file is None:
+        return "400"
+    try:
+        print("in try")
+        print(file)
+        file_path = "static/downloads/forge/" + file
+        return send_file(file_path, as_attachment=True)
+    except Exception as e:
+        # self.log.exception(e)
+        # self.Error(400)
+        print(str(e))
+        return "400"
 
 
 if __name__ == '__main__':
